8bc7a207362e78f27ce1b8318d711766716e7451
kahkah <146458402+KaroliniR@users.noreply.github.com>
2024-05-18 11:57:55 -0300

    Update Imovel.java

--- trabalho10.06/src/Imovel.java
+++ trabalho10.06/src/Imovel.java
@@ -1,76 +1,85 @@
 public class Imovel {
     private int codigo;
-    private int tipo;
-    private Localizacao localizacao;
-    private float preco;
-    private int numeroQuartos;
     private float areaConstruida;
     private float areaTotal;
+    private int numeroQuartos;
+    private int tipo; // 0 - Casa, 1 - Apartamento
+    private float preco;
+    private Endereco localizacao;
 
-    public Imovel(int codigo, int tipo, Localizacao localizacao, float preco, int numeroQuartos, float areaConstruida, float areaTotal) {
+    // Construtor
+    public Imovel(int codigo, float areaConstruida, float areaTotal, int numeroQuartos, int tipo, float preco, Endereco localizacao) {
         this.codigo = codigo;
-        this.tipo = tipo;
-        this.localizacao = localizacao;
-        this.preco = preco;
-        this.numeroQuartos = numeroQuartos;
         this.areaConstruida = areaConstruida;
         this.areaTotal = areaTotal;
+        this.numeroQuartos = numeroQuartos;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.localizacao = localizacao;
     }
 
-    public Imovel(int codigo2, float areaConstruida2, float areaTotal2, int numeroQuartos2, int tipo2, float preco2,
-            Endereco endereco) {
-        
-    }
-
+    // Getters e Setters
     public int getCodigo() {
         return codigo;
     }
 
-    public int getTipo() {
-        return tipo;
-    }
-
-    public Localizacao getLocalizacao() {
-        return localizacao;
-    }
-
-    public float getPreco() {
-        return preco;
-    }
-
-    public int getNumeroQuartos() {
-        return numeroQuartos;
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 
     public float getAreaConstruida() {
         return areaConstruida;
     }
 
-    public float getAreaTotal() {
-        return areaTotal;
-    }
-
     public void setAreaConstruida(float areaConstruida) {
         this.areaConstruida = areaConstruida;
+    }
+
+    public float getAreaTotal() {
+        return areaTotal;
     }
 
     public void setAreaTotal(float areaTotal) {
         this.areaTotal = areaTotal;
     }
 
+    public int getNumeroQuartos() {
+        return numeroQuartos;
+    }
+
     public void setNumeroQuartos(int numeroQuartos) {
         this.numeroQuartos = numeroQuartos;
+    }
+
+    public int getTipo() {
+        return tipo;
     }
 
     public void setTipo(int tipo) {
         this.tipo = tipo;
     }
 
+    public float getPreco() {
+        return preco;
+    }
+
     public void setPreco(float preco) {
         this.preco = preco;
     }
 
-    public void setLocalizacao(Localizacao localizacao) {
+    public Endereco getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(Endereco localizacao) {
         this.localizacao = localizacao;
+    }
+
+    // Método toString para exibir as informações do imóvel
+    @Override
+    public String toString() {
+        return "Código: " + codigo + ", Área Construída: " + areaConstruida + "m², Área Total: " + areaTotal + "m², " +
+               "Número de Quartos: " + numeroQuartos + ", Tipo: " + (tipo == 0 ? "Casa" : "Apartamento") + 
+               ", Preço: R$" + preco + ", " + localizacao;
     }
 }
